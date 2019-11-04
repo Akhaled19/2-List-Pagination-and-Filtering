d51fae4c131ea7d75ca2540e1fccef1f16e00048
Akhaled19 <akhaled@clarku.edu>
2019-11-04 18:02:38 -0500

    final comments

--- Unit-2-List-Pagination-and-Filtering/list_pagination_and_filtering-v1/js/script.js
+++ Unit-2-List-Pagination-and-Filtering/list_pagination_and_filtering-v1/js/script.js
@@ -11,7 +11,7 @@ function showPage(list, page) {
    //create two index to store the start and end index of the list to be displayed on a page
    const startIndex = (page * itemsPerPage) - itemsPerPage;
    const endIndex = page * itemsPerPage;
-//loop over the list starting with the first student at index 0
+//loop over the list starting with the first student at index 0 and displays the list of students
    for (let i = 0; i < list.length; i += 1) {
       if (i >= startIndex && i < endIndex) {
          list[i].style.display= 'block';
@@ -23,8 +23,9 @@ function showPage(list, page) {
 
 
 // Create the `appendPageLinks function` to generate, append, and add functionality to the pagination links.
-function appendPageLinks (list) {
-   const totalPages = Math.ceil(list.length / itemsPerPage);
+function appendPageLinks (list) { //list parameter represent the actual list of students
+   const totalPages = Math.ceil(list.length / itemsPerPage); //math to calculate how many pages needed for any given student list
+   //create and append 6 DOM elements for the pagination links following lines 119-137 html nested elements 
    const divPage = document.querySelector('.page');
    const newdiv = document.createElement('div');
    newdiv.className = 'pagination';
@@ -48,12 +49,16 @@ function appendPageLinks (list) {
 
       const alist = document.querySelectorAll('a'); 
 
+      //Add a click event listener to each A element by looping through alist 
      for (let n = 1; n <= alist.length; n+=1) {
         links.addEventListener('click', (e) => {
-           showPage(studentList, n);
+           showPage(studentList, n); 
+           //when a pagination link is clicked
            for (let v = 0; v < alist.length; v += 1) {
+              //active class name is removed 
              alist[v].className = '';
            }
+           //return active class name to the link using target property 
            e.target.className = 'active';
         });
      } 
@@ -61,7 +66,8 @@ function appendPageLinks (list) {
    }  
    
 }
-showPage(studentList, 1);
+//call the functions and pass them the global variables 
+showPage(studentList, 1);//add number 1 for the first page to be shown initially
 appendPageLinks(studentList);
 
 
